24f9088288efb492ae340f61a67bf8e7e50ad979
sambattalio <sambattalio99@gmail.com>
2020-02-03 23:44:17 -0500

    more matches per search... still need to get rid of garbage

--- popup.js
+++ popup.js
@@ -34,20 +34,55 @@ function findMatches(content, url, searchTerm, lastTag, chars){
     content = content.replace(/(<([^>]+)>)/ig,"");
     var n = content.search(searchTerm);
 
-    if (n != -1) {
-        var div = document.createElement('div');
-        div.setAttribute('class', 'matched');
+    var indices = getIndicesOf(searchTerm, content, false);
+    if (indices.length == 0) return;
+    // create base url
+    var div = document.createElement('div');
+    div.setAttribute('class', 'matched');
 
-        var a = document.createElement('a');
-        a.setAttribute('href', url);
-        a.innerHTML = url;
-        var para = document.createElement('p');
-        para.innerHTML = '\n' + content.substring(n - chars, n + chars) + '\n';
-
-        div.appendChild(a);
+    var a = document.createElement('a');
+    a.setAttribute('href', url);
+    a.innerHTML = url;
+    div.appendChild(a);
+    checkCloseIndices(indices, chars);
+    console.log(indices);
+    // Grab strings
+    for (n in indices) {
+        let para = document.createElement('p');
+        para.innerHTML = '\n' + content.substring(indices[n] - chars, indices[n] + chars) + '\n';
         div.appendChild(para);
+    } 
+    lastTag.parentNode.insertBefore(div, lastTag.nextSibling);
+    lastTag = div;
+}
 
-        lastTag.parentNode.insertBefore(div, lastTag.nextSibling);
-        lastTag = div;
+// combines indices if too close to eachother
+function checkCloseIndices(indices, chars) {
+    var last_i = indices.length - 1;
+    var i = indices.length - 1;
+    while (i--) {
+        if (indices[last_i] - indices[i] < chars * 2) {
+            indices.splice(i, 1);
+            continue;
+        }
+        last_i = i;
     }
+}
+
+// Thanks stack overflow!
+function getIndicesOf(searchStr, str, caseSensitive) {
+    var searchStrLen = searchStr.length;
+    if (searchStrLen == 0) {
+        return [];
+    }
+    var startIndex = 0, index, indices = [];
+    if (!caseSensitive) {
+        str = str.toLowerCase();
+        searchStr = searchStr.toLowerCase();
+    }
+    while ((index = str.indexOf(searchStr, startIndex)) > -1) {
+        indices.push(index);
+        startIndex = index + searchStrLen;
+    }
+    return indices;
 }
